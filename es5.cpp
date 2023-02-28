120bcd791c40a66d92164120c2e8ada362a506b3
Marco Boccia <pokermark95@live.it>
2023-02-28 12:42:36 +0100

    finito es5 con funzione

---
+++ es5.cpp
@@ -0,0 +1,30 @@
+/*
+Scrivi una funzione che prenda in input un array di interi e la sua lunghezza e restituisca true se l'array è ordinato in ordine crescente e false altrimenti.
+*/
+
+#include <iostream>
+#include <string>
+#include <array>
+using namespace std;
+
+bool isOrdered(int arr[],int len){
+    for(int i = 0; i < len -1;i++){
+        if (arr[i] > arr[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int arr[] = {1,2,5,6,7};
+    int len =sizeof(arr)/sizeof(arr[0]);
+    bool order =isOrdered(arr,len);
+    if (order){
+        cout<<"L array e in ordine crescente"<<endl;
+    }else
+    {
+        cout<<"L array non e in ordine crescente"<<endl;
+    }
+    return 0;
+}
