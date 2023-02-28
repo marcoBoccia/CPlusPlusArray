9aa5b4b0b4db7300aa399108496643ade44e685d
Marco Boccia <pokermark95@live.it>
2023-02-28 11:58:57 +0100

    finito es3 con funzione

--- es3.cpp
+++ es3.cpp
@@ -7,10 +7,28 @@ Scrivi una funzione che prenda in input un array di interi e la sua lunghezza e 
 #include <array>
 using namespace std;
 
+/* solution 1
 int main(){
     int arr [5] = {0,1,2,3,4};
     int sum = 0;
     for (int i = 0; i <= sizeof(arr)/sizeof(arr[0]); i++)
     sum += arr[i];
     cout << sum << endl;
+}
+*/
+
+// solution 2
+int sum(int arr [], int len){
+    int result = 0;
+    for (int i = 0; i < len; i++){
+    result += arr[i];
+    }
+    return result;
+}
+
+int main(){
+    int arr[] = {0,1,2,3,4};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    cout <<"La somma degli elementi dell'array è: " << sum(arr,len)<< endl;
+    return 0;
 }
